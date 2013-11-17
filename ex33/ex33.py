e802440aa52aa9dabd729192b7a78e07ff3c687e
Charles Baker <charleshbaker@gmail.com>
2013-11-17 15:44:10 -0500

    Put while loop into a funtion w/ an argument for length

--- ex33/ex33.py
+++ ex33/ex33.py
@@ -1,15 +1,19 @@
 
+def numeric_list_builder(length):
+    i = 0
+    numbers = []
+    while i < length:
+        print "At the top i is %d" % i
+        numbers.append(i)
+        i = i + 1
+        print "Numbers now:  ", numbers
+        print "At the bottom i is %d" % i
+    return numbers
 
-i = 0
-numbers = []
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
-    i = i + 1
-    print "Numbers now:  ", numbers
-    print "At the bottom i is %d" % i
+myList = numeric_list_builder(10)
+
 print "The numbers:  "
-for num in numbers:
+for num in myList:
     print num
 
 
