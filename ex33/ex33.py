21d9887581b1a13dc2b74ddad20d9e93d373341f
Charles Baker <charleshbaker@gmail.com>
2013-11-17 15:46:55 -0500

    Added an increment variable to the ex33.py function.

--- ex33/ex33.py
+++ ex33/ex33.py
@@ -1,16 +1,16 @@
 
-def numeric_list_builder(length):
+def numeric_list_builder(length, increment):
     i = 0
     numbers = []
     while i < length:
         print "At the top i is %d" % i
         numbers.append(i)
-        i = i + 1
+        i = i + increment
         print "Numbers now:  ", numbers
         print "At the bottom i is %d" % i
     return numbers
 
-myList = numeric_list_builder(10)
+myList = numeric_list_builder(10, 2)
 
 print "The numbers:  "
 for num in myList:
