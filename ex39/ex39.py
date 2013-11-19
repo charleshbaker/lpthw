deced526de5564553ddb9f091b633547cf2a71ef
Charles Baker <charleshbaker@gmail.com>
2013-11-19 15:51:15 -0500

    Added some examples of "safer" programming using default values.

--- ex39/ex39.py
+++ ex39/ex39.py
@@ -54,4 +54,16 @@ for state, abbrev in states.items():
     print "%s state is abbreviated %s and has city %s" % (
             state, abbrev, cities[abbrev])
 
+print '-' * 10
+
+# safely get an abbreviation by state that might not be there
+state = states.get('Texas', None)
+
+if not state:
+    print "Sorry, no Texas."
+
+# get a city with a default value
+city = cities.get('TX', 'Does Not Exist')
+print "The city for state 'TX' is:  %s" % city
+
 
