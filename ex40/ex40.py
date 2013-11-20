bc530c74e39088a9ccf63d4cdc6b37a14f839d96
Charles Baker <charleshbaker@gmail.com>
2013-11-20 10:15:58 -0500

    Created a Song object using a list variable.

--- ex40/ex40.py
+++ ex40/ex40.py
@@ -29,3 +29,11 @@ lyric = "Call me big poppa!"
 biggie = Song(lyric)
 
 biggie.sing_me_a_song()
+
+jordan = ["Jordan river is chilly and cold",
+          "It chills the body",
+          "But not the soul"]
+
+spiritual = Song(jordan)
+
+spiritual.sing_me_a_song()
